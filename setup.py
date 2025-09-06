6334e4a4d66bd86b79a4e65797234cc627c66792
romanvolkov650 <romanvolkov650@gmail.com>
2025-09-06 21:21:31 +0300

    version bump

--- setup.py
+++ setup.py
@@ -1,27 +1,32 @@
 import sys
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 if sys.version_info < (3, 12):
     sys.exit("Python < 3.12 is not supported")
 
-version = '1.2.1'
+version = "1.2.2"
 
 setup(
-    name='steampy',
-    packages=['steampy', 'test', 'examples'],
+    name="steampy",
+    packages=find_packages(
+        include=["steampy", "steampy.*"],
+        exclude=("tests", "tests.*", "test", "test.*", "examples", "examples.*"),
+    ),
     version=version,
-    description='A Steam lib for trade automation',
-    author='Michał Bukowski',
-    author_email='gigibukson@gmail.com',
-    license='MIT',
-    url='https://github.com/bukson/steampy',
-    download_url='https://github.com/bukson/steampy/tarball/' + version,
-    keywords=['steam', 'trade'],
+    description="A Steam lib for trade automation",
+    author="Michał Bukowski",
+    author_email="gigibukson@gmail.com",
+    license="MIT",
+    url="https://github.com/bukson/steampy",
+    download_url="https://github.com/bukson/steampy/tarball/" + version,
+    keywords=["steam", "trade"],
     classifiers=[],
-    install_requires=[
-        "requests",
-        "beautifulsoup4",
-        "rsa",
-    ],
+    install_requires=["requests", "beautifulsoup4", "rsa", "protobuf>=5"],
+    include_package_data=True,
+    package_data={
+        "steampy": [
+            "generated/**/*.pyi",  # если генерируешь типовые заглушки
+        ]
+    },
 )
