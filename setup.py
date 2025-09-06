e58f7d0036f9c4797006f356c21f5767ce56d8ec
romanvolkov650 <romanvolkov650@gmail.com>
2025-09-06 21:34:23 +0300

    version bump

--- setup.py
+++ setup.py
@@ -5,28 +5,24 @@ from setuptools import setup, find_packages
 if sys.version_info < (3, 12):
     sys.exit("Python < 3.12 is not supported")
 
-version = "1.2.2"
+version = "1.2.4"
 
 setup(
     name="steampy",
-    packages=find_packages(
-        include=["steampy", "steampy.*"],
-        exclude=("tests", "tests.*", "test", "test.*", "examples", "examples.*"),
-    ),
     version=version,
     description="A Steam lib for trade automation",
     author="Michał Bukowski",
     author_email="gigibukson@gmail.com",
     license="MIT",
     url="https://github.com/bukson/steampy",
-    download_url="https://github.com/bukson/steampy/tarball/" + version,
-    keywords=["steam", "trade"],
-    classifiers=[],
+    packages=find_packages(
+        include=["steampy", "steampy.*"],
+        exclude=("tests", "tests.*", "test", "test.*", "examples", "examples.*"),
+    ),
     install_requires=["requests", "beautifulsoup4", "rsa", "protobuf>=5"],
     include_package_data=True,
-    package_data={
-        "steampy": [
-            "generated/**/*.pyi",  # если генерируешь типовые заглушки
-        ]
-    },
+    package_data={"steampy": ["generated/**/*.pyi"]},
+    python_requires=">=3.12",
+    keywords=["steam", "trade"],
+    classifiers=[],
 )
